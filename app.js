46e44a3d7d3023a159263f726b32f1f2ba3792d2
Pietro Maciel <pietroomv150206@gmail.com>
2023-07-23 19:47:39 -0300

    changes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const shortid = require("shortid");
-const cache = require("memory-cache");
+const { Pool } = require("pg");
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(express.json());
 
+// Create a PostgreSQL pool to handle database connections
+const pool = new Pool({
+  connectionString:
+    "postgres://postgres:password@localhost:5432/url_shortener_database",
+});
+
+// Get the Cyclic URL from the environment variable
+const cyclicUrl = process.env.CYCLIC_URL || `http://localhost:${port}`;
+
 // Endpoint to shorten a URL
-app.post("/shorten", (req, res) => {
-  const { url } = req.body;
+app.post("/shorten", async (req, res) => {
+  const { url } = req.query;
 
   if (!url) {
     return res.status(400).json({ error: "URL parameter is missing." });
@@ -17,10 +26,17 @@ app.post("/shorten", (req, res) => {
 
   try {
     const shortId = shortid.generate(); // Generate a random short ID
-    const urlMapping = { shortId, url }; // URL mapping object
-    cache.put(shortId, urlMapping); // Store the mapping in the cache
 
-    const shortUrl = `http://localhost:${port}/${shortId}`;
+    // Store the mapping in the database
+    const insertQuery =
+      `INSERT INTO urls (shortId, url) VALUES ($1, $2)`;
+    const result = await pool.query(insertQuery, [shortId, url]);
+
+    if (result.rows.length === 0) {
+      throw new Error("Error storing URL in the database.");
+    }
+
+    const shortUrl = `${cyclicUrl}/${shortId}`; // Use the Cyclic URL
     res.json({ shortUrl });
   } catch (error) {
     console.error("Error generating short ID:", error);
@@ -31,15 +47,25 @@ app.post("/shorten", (req, res) => {
 });
 
 // Endpoint to redirect to the original URL
-app.get("/:shortId", (req, res) => {
+app.get("/:shortId", async (req, res) => {
   const { shortId } = req.params;
-  const urlMapping = cache.get(shortId);
 
-  if (!urlMapping) {
-    return res.status(404).json({ error: "Short URL not found." });
+  try {
+    // Retrieve the URL from the database
+    const selectQuery = "SELECT url FROM urls WHERE shortId = $1";
+    const result = await pool.query(selectQuery, [shortId]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Short URL not found." });
+    }
+
+    res.redirect(result.rows[0].url);
+  } catch (error) {
+    console.error("Error retrieving URL from the database:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving the URL." });
   }
-
-  res.redirect(urlMapping.url);
 });
 
 app.listen(port, () => {
