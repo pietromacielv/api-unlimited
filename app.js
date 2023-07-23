27430dfcb761b823b6a0947c682bda5f760f9f28
Pietro Maciel <pietroomv150206@gmail.com>
2023-07-23 19:50:09 -0300

    change

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const shortid = require("shortid");
-const { Pool } = require("pg");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-// Create a PostgreSQL pool to handle database connections
-const pool = new Pool({
-  connectionString:
-    "postgres://postgres:password@localhost:5432/url_shortener_database",
-});
-
 // Get the Cyclic URL from the environment variable
 const cyclicUrl = process.env.CYCLIC_URL || `http://localhost:${port}`;
 
+// Create a variable to store the short URLs in Cyclic
+let urlMap = {};
+
 // Endpoint to shorten a URL
 app.post("/shorten", async (req, res) => {
-  const { url } = req.query;
+  const { url } = req.body;
 
   if (!url) {
     return res.status(400).json({ error: "URL parameter is missing." });
@@ -27,14 +23,8 @@ app.post("/shorten", async (req, res) => {
   try {
     const shortId = shortid.generate(); // Generate a random short ID
 
-    // Store the mapping in the database
-    const insertQuery =
-      `INSERT INTO urls (shortId, url) VALUES ($1, $2)`;
-    const result = await pool.query(insertQuery, [shortId, url]);
-
-    if (result.rows.length === 0) {
-      throw new Error("Error storing URL in the database.");
-    }
+    // Store the mapping in the Cyclic environment variable
+    urlMap[shortId] = url;
 
     const shortUrl = `${cyclicUrl}/${shortId}`; // Use the Cyclic URL
     res.json({ shortUrl });
@@ -50,22 +40,14 @@ app.post("/shorten", async (req, res) => {
 app.get("/:shortId", async (req, res) => {
   const { shortId } = req.params;
 
-  try {
-    // Retrieve the URL from the database
-    const selectQuery = "SELECT url FROM urls WHERE shortId = $1";
-    const result = await pool.query(selectQuery, [shortId]);
+  // Retrieve the URL from the Cyclic environment variable
+  const url = urlMap[shortId];
 
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Short URL not found." });
-    }
-
-    res.redirect(result.rows[0].url);
-  } catch (error) {
-    console.error("Error retrieving URL from the database:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while retrieving the URL." });
+  if (!url) {
+    return res.status(404).json({ error: "Short URL not found." });
   }
+
+  res.redirect(url);
 });
 
 app.listen(port, () => {
