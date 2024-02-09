b3b7a61d4b7ebfe0bfa5da210467e2d716e9a9b3
Pietro Maciel <pietroomv150206@gmail.com>
2024-02-09 12:34:22 -0300

    refactor: remove unnecessary route

--- src/routes/ai.routes.js
+++ src/routes/ai.routes.js
@@ -31,18 +31,4 @@ aiRoutes.post("/ai", async (req, res) => {
   }
 });
 
-aiRoutes.get("/ai/reset", async (req, res) => {
-  try {
-    new Bard(process.env.COOKIE_KEY);
-    return res.status(200).json({
-      success: "Resetado com sucesso.",
-    });
-  } catch (error) {
-    console.error("Error resetting AI:", error);
-    return res.status(500).json({
-      error: "Error resetting AI",
-    });
-  }
-});
-
 export default aiRoutes;
