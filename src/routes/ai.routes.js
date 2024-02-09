fcef9c1993140bdf1b469877d9720a763284ef68
Pietro Maciel <pietroomv150206@gmail.com>
2024-02-09 14:28:06 -0300

    refactor: set safetySettings to none

--- src/routes/ai.routes.js
+++ src/routes/ai.routes.js
@@ -1,15 +1,34 @@
 import express from "express";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
 const aiRoutes = express.Router();
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyBx5-agHvSTwRYgiPnxMW2t0sCyMxKfM_k");
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-const chat = model.startChat({
-  generationConfig: {
-    maxOutputTokens: 2000,
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
   },
-});
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
+
+const chat = model.startChat({ safetySettings });
 
 aiRoutes.post("/ai", async (req, res) => {
   const { content } = req.body;
@@ -21,6 +40,7 @@ aiRoutes.post("/ai", async (req, res) => {
   try {
     const result = await chat.sendMessage(content);
     const response = await result.response;
+    logFields(response);
     const text = response.text();
     res.json({ text });
   } catch (error) {
